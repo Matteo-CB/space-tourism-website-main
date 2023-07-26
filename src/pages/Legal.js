8ec5a422f22f4db2c8a5ee07a4c3ef2d9175f7ca
Matteo Chanté-Biyikli <130081405+Matteo-CB@users.noreply.github.com>
2023-07-26 06:48:01 +0200

    Update Legal.js

--- src/pages/Legal.js
+++ src/pages/Legal.js
@@ -42,23 +42,20 @@ const Legal = () => {
     <Header />
     <div className="legal">
 
-      <h1>Legal Notice</h1>
-
-      <h2>Publisher Information:</h2>
+      
       <p>
-        Name: Matteo Chanté-Biyikli<br />
-        Address: 9 Clos Justin, 84800 L'Isle-sur-la-Sorgue, France<br />
-        Phone Number: 06 41 37 84 41<br />
-        Email Address: matteochantebiyikli@gmail.com
+        Ce site est édité par Matteo Chante-Biyikli, résidant 9 clos justin, 84800 l'isle sur la sorgue.
+
+Téléphone : 0641378441.
+
+Courrier électronique : matteochantebiyikli@gmail.com.
+
+Le directeur de la publication est Matteo Chante-Biyikli.
+
+Ce site est hébergé par la société Vercel Inc., située 340 S Lemon Ave #4133 Walnut, CA 91789, et joignable au (559) 288-7060.
+
       </p>
 
-      <h2>Website Host:</h2>
-      <p>
-        Name: Vercel<br />
-        
-      </p>
-
-      <h2>Intellectual Property:</h2>
       <p>
         The content of this website, including text, images, videos, logos, and graphics from the Frontend Mentor
         project, is protected by French and international copyright and intellectual property laws. Any unauthorized
@@ -66,24 +63,6 @@ const Legal = () => {
         permission of the owner.
       </p>
 
-      <h2>External Links:</h2>
-      <p>
-        This website may contain links to third-party websites. We assume no responsibility for the content or privacy
-        practices of these websites.
-      </p>
-
-      <h2>Limitation of Liability:</h2>
-      <p>
-        We strive to provide accurate and up-to-date information on this website. However, we cannot guarantee the
-        accuracy, completeness, or relevance of the information provided. We disclaim any liability for the use or
-        interpretation of the information on this site.
-      </p>
-
-      <h2>Changes to the Legal Notice:</h2>
-      <p>
-        We reserve the right to modify these legal notices at any time without notice. It is your responsibility to
-        regularly review this page for any updates.
-      </p>
     </div>
     </>
   );
